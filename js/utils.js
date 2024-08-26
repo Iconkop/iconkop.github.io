dc300f934c56d4116577f3daa7b7ee5179b2975e
iconkop <shenzhan22@gmail.com>
2024-08-26 17:56:29 +0800

    Site updated: 2024-08-26 17:56:29

--- js/utils.js
+++ js/utils.js
@@ -1,245 +1,202 @@
-/* global Fluid, CONFIG */
-
-window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
-
-Fluid.utils = {
-
-  listenScroll: function(callback) {
-    var dbc = new Debouncer(callback);
-    window.addEventListener('scroll', dbc, false);
-    dbc.handleEvent();
-    return dbc;
-  },
-
-  unlistenScroll: function(callback) {
-    window.removeEventListener('scroll', callback);
-  },
-
-  listenDOMLoaded(callback) {
-    if (document.readyState !== 'loading') {
-      callback();
-    } else {
-      document.addEventListener('DOMContentLoaded', function () {
-        callback();
-      });
-    }
-  },
-
-  scrollToElement: function(target, offset) {
-    var of = jQuery(target).offset();
-    if (of) {
-      jQuery('html,body').animate({
-        scrollTop: of.top + (offset || 0),
-        easing   : 'swing'
-      });
-    }
-  },
-
-  elementVisible: function(element, offsetFactor) {
-    offsetFactor = offsetFactor && offsetFactor >= 0 ? offsetFactor : 0;
-    var rect = element.getBoundingClientRect();
-    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-    return (
-      (rect.top >= 0 && rect.top <= viewportHeight * (1 + offsetFactor) + rect.height / 2) ||
-      (rect.bottom >= 0 && rect.bottom <= viewportHeight * (1 + offsetFactor) + rect.height / 2)
-    );
-  },
-
-  waitElementVisible: function(selectorOrElement, callback, offsetFactor) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window))
-      || (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    if (!runningOnBrowser || isBot) {
-      return;
-    }
-
-    offsetFactor = offsetFactor && offsetFactor >= 0 ? offsetFactor : 0;
-
-    function waitInViewport(element) {
-      Fluid.utils.listenDOMLoaded(function() {
-        if (Fluid.utils.elementVisible(element, offsetFactor)) {
-          callback();
-          return;
-        }
-        if ('IntersectionObserver' in window) {
-          var io = new IntersectionObserver(function(entries, ob) {
-            if (entries[0].isIntersecting) {
-              callback();
-              ob.disconnect();
+(() => {
+    const utilsFn = {
+        throttle: (func, wait, {leading = true, trailing = true} = {}) => {
+            let timeout, previous = 0;
+            const later = (context, args) => {
+                timeout = previous = leading === false ? 0 : Date.now();
+                func.apply(context, args);
+            };
+            return function () {
+                const now = Date.now();
+                if (!previous && leading === false) previous = now;
+                const remaining = wait - (now - previous);
+                if (remaining <= 0 || remaining > wait) {
+                    if (timeout) {
+                        clearTimeout(timeout);
+                        timeout = null;
+                    }
+                    later(this, arguments);
+                } else if (!timeout && trailing !== false) {
+                    timeout = setTimeout(() => later(this, arguments), remaining);
+                }
+            };
+        },
+        fadeIn: (ele, time) => ele.style.cssText = `display:block;animation: to_show ${time}s`,
+        fadeOut: (ele, time) => {
+            const resetStyles = () => {
+                ele.style.cssText = "display: none; animation: '' ";
+                ele.removeEventListener('animationend', resetStyles);
+            };
+            ele.addEventListener('animationend', resetStyles);
+            ele.style.animation = `to_hide ${time}s`;
+        },
+        sidebarPaddingR: () => {
+            const paddingRight = window.innerWidth - document.body.clientWidth;
+            if (paddingRight > 0) {
+                document.body.style.paddingRight = `${paddingRight}px`;
             }
-          }, {
-            threshold : [0],
-            rootMargin: (window.innerHeight || document.documentElement.clientHeight) * offsetFactor + 'px'
-          });
-          io.observe(element);
-        } else {
-          var wrapper = Fluid.utils.listenScroll(function() {
-            if (Fluid.utils.elementVisible(element, offsetFactor)) {
-              Fluid.utils.unlistenScroll(wrapper);
-              callback();
+        },
+        snackbarShow: (text, showAction = false, duration = 5000) => {
+            document.styleSheets[0].addRule(':root', `--efu-snackbar-time:${duration}ms!important`);
+            Snackbar.show({
+                text,
+                showAction,
+                duration,
+                pos: 'top-center'
+            });
+        },
+        copy: async (text) => {
+            const message = await navigator.clipboard.writeText(text)
+                .then(() => GLOBAL_CONFIG.lang.copy.success)
+                .catch(() => GLOBAL_CONFIG.lang.copy.error);
+            utils.snackbarShow(message, false, 2000);
+        },
+        getEleTop: ele => {
+            let actualTop = ele.offsetTop;
+            while (ele.offsetParent) {
+                ele = ele.offsetParent;
+                actualTop += ele.offsetTop;
             }
-          });
-        }
-      });
+            return actualTop;
+        },
+        siblings: (ele, selector) => {
+            return [...ele.parentNode.children].filter((child) => {
+                if (selector) {
+                    return child !== ele && child.matches(selector)
+                }
+                return child !== ele
+            })
+        },
+        randomNum: (length) => {
+            return Math.floor(Math.random() * length)
+        },
+        timeDiff: (timeObj, today) => {
+            const timeDiff = today.getTime() - timeObj.getTime();
+            return Math.floor(timeDiff / (1000 * 3600 * 24));
+        },
+        scrollToDest: (pos, time = 500) => {
+            const currentPos = window.pageYOffset;
+            const isNavFixed = document.getElementById('page-header').classList.contains('nav-fixed');
+            pos = currentPos > pos || isNavFixed ? pos - 70 : pos;
+
+            if ('scrollBehavior' in document.documentElement.style) {
+                window.scrollTo({top: pos, behavior: 'smooth'});
+                return;
+            }
+
+            const distance = pos - currentPos;
+            const step = currentTime => {
+                const start = start || currentTime;
+                const progress = currentTime - start;
+
+                if (progress < time) {
+                    window.scrollTo(0, currentPos + distance * progress / time);
+                    window.requestAnimationFrame(step);
+                } else {
+                    window.scrollTo(0, pos);
+                }
+            };
+
+            window.requestAnimationFrame(step);
+        },
+        isMobile: () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+        isHidden: e => 0 === e.offsetHeight && 0 === e.offsetWidth,
+        animateIn: (ele, text) => {
+            Object.assign(ele.style, {display: 'block', animation: text});
+        },
+        animateOut: (ele, text) => {
+            const resetAnimation = () => {
+                ele.style.display = '';
+                ele.style.animation = '';
+                ele.removeEventListener('animationend', resetAnimation);
+            };
+            ele.addEventListener('animationend', resetAnimation);
+            ele.style.animation = text;
+        },
+        wrap: (selector, eleType, options) => {
+            const createEle = document.createElement(eleType)
+            for (const [key, value] of Object.entries(options)) {
+                createEle.setAttribute(key, value)
+            }
+            selector.parentNode.insertBefore(createEle, selector)
+            createEle.appendChild(selector)
+        },
+        lazyloadImg: () => {
+            window.lazyLoadInstance = new LazyLoad({
+                elements_selector: 'img',
+                threshold: 0,
+                data_src: 'lazy-src',
+                callback_error: img => img.src = GLOBAL_CONFIG.lazyload.error
+            });
+        },
+        lightbox: function (selector) {
+            const lightboxType = GLOBAL_CONFIG.lightbox;
+            const options = {
+                class: 'fancybox',
+                'data-fancybox': 'gallery',
+            };
+
+            if (lightboxType === 'mediumZoom') {
+                mediumZoom && mediumZoom(selector, {background: "var(--efu-card-bg)"});
+            } else if (lightboxType === 'fancybox') {
+                selector.forEach(i => {
+                    if (i.parentNode.tagName !== 'A') {
+                        options.href = options['data-thumb'] = i.dataset.lazySrc || i.src;
+                        options['data-caption'] = i.title || i.alt || '';
+                        utils.wrap(i, 'a', options);
+                    }
+                });
+
+                if (!window.fancyboxRun) {
+                    Fancybox.bind('[data-fancybox]', {
+                        Hash: false,
+                        animated: true,
+                        Thumbs: {showOnStart: false},
+                        Images: {Panzoom: {maxScale: 4}},
+                        Carousel: {transition: 'slide'},
+                        Toolbar: {
+                            display: {
+                                left: ['infobar'],
+                                middle: ['zoomIn', 'zoomOut', 'toggle1to1', 'rotateCCW', 'rotateCW', 'flipX', 'flipY'],
+                                right: ['slideshow', 'thumbs', 'close']
+                            }
+                        },
+                    });
+                    window.fancyboxRun = true;
+                }
+            }
+        },
+        diffDate: (d, more = false) => {
+            const dateNow = new Date();
+            const datePost = new Date(d);
+            const dateDiff = dateNow - datePost;
+            const minute = 60000;
+            const hour = 3600000;
+            const day = 86400000;
+            const month = 2592000000;
+            const {time} = GLOBAL_CONFIG.lang;
+            const dayCount = Math.floor(dateDiff / day)
+            if (!more) return dayCount
+            const minuteCount = Math.floor(dateDiff / minute)
+            const hourCount = Math.floor(dateDiff / hour)
+            const monthCount = Math.floor(dateDiff / month)
+            if (monthCount > 12) return datePost.toISOString().slice(0, 10)
+            if (monthCount >= 1) return `${monthCount} ${time.month}`
+            if (dayCount >= 1) return `${dayCount} ${time.day}`
+            if (hourCount >= 1) return `${hourCount} ${time.hour}`
+            if (minuteCount >= 1) return `${minuteCount} ${time.min}`
+            return time.just
+        },
+        loadComment: (dom, callback) => {
+            const observerItem = 'IntersectionObserver' in window ? new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting) {
+                    callback();
+                    observerItem.disconnect();
+                }
+            }, {threshold: [0]}) : null;
+
+            observerItem ? observerItem.observe(dom) : callback();
+        },
     }
-
-    if (typeof selectorOrElement === 'string') {
-      this.waitElementLoaded(selectorOrElement, function(element) {
-        waitInViewport(element);
-      });
-    } else {
-      waitInViewport(selectorOrElement);
-    }
-  },
-
-  waitElementLoaded: function(selector, callback) {
-    var runningOnBrowser = typeof window !== 'undefined';
-    var isBot = (runningOnBrowser && !('onscroll' in window))
-      || (typeof navigator !== 'undefined' && /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(navigator.userAgent));
-    if (!runningOnBrowser || isBot) {
-      return;
-    }
-
-    if ('MutationObserver' in window) {
-      var mo = new MutationObserver(function(records, ob) {
-        var ele = document.querySelector(selector);
-        if (ele) {
-          callback(ele);
-          ob.disconnect();
-        }
-      });
-      mo.observe(document, { childList: true, subtree: true });
-    } else {
-      Fluid.utils.listenDOMLoaded(function() {
-        var waitLoop = function() {
-          var ele = document.querySelector(selector);
-          if (ele) {
-            callback(ele);
-          } else {
-            setTimeout(waitLoop, 100);
-          }
-        };
-        waitLoop();
-      });
-    }
-  },
-
-  createScript: function(url, onload) {
-    var s = document.createElement('script');
-    s.setAttribute('src', url);
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('charset', 'UTF-8');
-    s.async = false;
-    if (typeof onload === 'function') {
-      if (window.attachEvent) {
-        s.onreadystatechange = function() {
-          var e = s.readyState;
-          if (e === 'loaded' || e === 'complete') {
-            s.onreadystatechange = null;
-            onload();
-          }
-        };
-      } else {
-        s.onload = onload;
-      }
-    }
-    var ss = document.getElementsByTagName('script');
-    var e = ss.length > 0 ? ss[ss.length - 1] : document.head || document.documentElement;
-    e.parentNode.insertBefore(s, e.nextSibling);
-  },
-
-  createCssLink: function(url) {
-    var l = document.createElement('link');
-    l.setAttribute('rel', 'stylesheet');
-    l.setAttribute('type', 'text/css');
-    l.setAttribute('href', url);
-    var e = document.getElementsByTagName('link')[0]
-      || document.getElementsByTagName('head')[0]
-      || document.head || document.documentElement;
-    e.parentNode.insertBefore(l, e);
-  },
-
-  loadComments: function(selector, loadFunc) {
-    var ele = document.querySelector('#comments[lazyload]');
-    if (ele) {
-      var callback = function() {
-        loadFunc();
-        ele.removeAttribute('lazyload');
-      };
-      Fluid.utils.waitElementVisible(selector, callback, CONFIG.lazyload.offset_factor);
-    } else {
-      loadFunc();
-    }
-  },
-
-  getBackgroundLightness(selectorOrElement) {
-    var ele = selectorOrElement;
-    if (typeof selectorOrElement === 'string') {
-      ele = document.querySelector(selectorOrElement);
-    }
-    var view = ele.ownerDocument.defaultView;
-    if (!view) {
-      view = window;
-    }
-    var rgbArr = view.getComputedStyle(ele).backgroundColor.replace(/rgba*\(/, '').replace(')', '').split(/,\s*/);
-    if (rgbArr.length < 3) {
-      return 0;
-    }
-    var colorCast = (0.213 * rgbArr[0]) + (0.715 * rgbArr[1]) + (0.072 * rgbArr[2]);
-    return colorCast === 0 || colorCast > 255 / 2 ? 1 : -1;
-  },
-
-  retry(handler, interval, times) {
-    if (times <= 0) {
-      return;
-    }
-    var next = function() {
-      if (--times >= 0 && !handler()) {
-        setTimeout(next, interval);
-      }
-    };
-    setTimeout(next, interval);
-  }
-
-};
-
-/**
- * Handles debouncing of events via requestAnimationFrame
- * @see http://www.html5rocks.com/en/tutorials/speed/animations/
- * @param {Function} callback The callback to handle whichever event
- */
-function Debouncer(callback) {
-  this.callback = callback;
-  this.ticking = false;
-}
-
-Debouncer.prototype = {
-  constructor: Debouncer,
-
-  /**
-   * dispatches the event to the supplied callback
-   * @private
-   */
-  update: function() {
-    this.callback && this.callback();
-    this.ticking = false;
-  },
-
-  /**
-   * ensures events don't get stacked
-   * @private
-   */
-  requestTick: function() {
-    if (!this.ticking) {
-      requestAnimationFrame(this.rafCallback || (this.rafCallback = this.update.bind(this)));
-      this.ticking = true;
-    }
-  },
-
-  /**
-   * Attach this as the event listeners
-   */
-  handleEvent: function() {
-    this.requestTick();
-  }
-};
+    window.utils = {...window.utils, ...utilsFn};
+})()
